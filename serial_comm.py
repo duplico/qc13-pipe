91d0bbbb49400b3825464d838529979bff745358
George Louthan <duplico@dupli.co>
2016-07-31 21:15:00 +0000

    Refactor.

--- serial_comm.py
+++ serial_comm.py
@@ -1,3 +1,5 @@
+from collections import namedtuple, OrderedDict
+
 import crcmod
 import serial
 from struct import pack, unpack
@@ -6,7 +8,7 @@ from bitstruct import unpack as bitunpack
 
 achievements_awarded = [55, 56]
 
-hats = {
+hats_unordered = {
     0: ("DUPLiCO's special friend", "Be chosen."),
     1: ("akio's special friend", "Be chosen."),
     2: ("Tprophet's special friend", "Be chosen."),
@@ -59,47 +61,17 @@ hats = {
     56: ("Handler", "Volunteer!"),
 }
 
-# typedef struct {
-    # uint8_t proto_version;
-    # uint8_t from_addr;
-    # uint8_t hat_award_id;
-    # uint8_t camo_id;
-    # uint16_t flags;
-    # uint64_t achievements;
-    # uint8_t seen_count, uber_seen_count, odh_seen_count;
-    # uint8_t mate_count, uber_mate_count, odh_mate_count;
-    # uint16_t crc16;
-# } matepayload;
+hats = OrderedDict(sorted(hats_unordered.items(), key=lambda t: t[0]))
 
-#define M_HAT_AWARD BIT1 // Award = PUSH hat. From badge.
-#define M_HAT_AWARD_ACK BIT2
-#define M_HAT_AWARD_NACK BIT3
-#define M_INK BIT4
-#define M_HAT_HOLDER BIT5
-#define M_RST BIT6
-#define M_BADGE_HAS_CLAIMED_HAT BIT7 // comes from badge
-#define M_HANDLER_ON_DUTY BIT8
-#define M_PIPE BIT9 // 0=badge; 1=pipe
-#define M_HAT_CLAIM_FROM_PIPE BITA // comes from pipe
-#define M_REPRINT_HAT BITB
-#define M_BESTOW_GILD BITF
-
-# May need to pad, given the 2-byte words in MSP430.
-# sync bytes: 0xA0, 0xFF, 0x00, 0xAA
-#  these happen at the beginning.
-# They're NOT included in this format:
-# '<' = little-endian
-# '>' = big-endian
 mating_format = '<BBBBHQBBBBBBH'
 mating_format_nocrc = '<BBBBHQBBBBBB'
+flag_format = 'p4u1u1u1u1u1u1u1u1u1u1u1p1'
 
-
-# print mate_payload_tuple
+MatePayloadRaw = namedtuple('MatePayloadRaw', 'proto_version from_addr hat_award_id camo_id flags achievements seen_count uber_seen_count odh_seen_count mate_count uber_mate_count odh_mate_count crc16')
+Flags = namedtuple('Flags', 'f_reprint f_hat_claim_from_pipe f_pipe f_handler f_badge_has_claimed_hat f_rst f_hat_holder f_ink f_nack f_ack f_award')
+MatePayload = namedtuple('MatePayload', MatePayloadRaw._fields + Flags._fields)
 
 sync_bytes = [0xA0, 0xFF, 0x00, 0xAA]
-sync_position = 0
-payload_len = 22
-payload = ''
 
 DEDICATED_BASE_ID = 254
 
@@ -107,116 +79,86 @@ crc_check = crcmod.mkCrcFun(0b10001000000100001, rev=False)
 
 # When we send, f_pipe must be set, and out ID must be DEDICATED_BASE_ID
 
-def send_pipe_claim_hat(hat_id):
-    # first entry is padding...
-    flags_bits = (0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    flags_bytes = bitpack('u4u1u1u1u1u1u1u1u1u1u1u1u1', *flags_bits)
+def send_pipe_message(rst, hat_id):
+    hid = 0xff
+    hawd = 0
+    hclm = 0
+    if hat_id is not None:
+        hid = hat_id
+        hawd = 1
+        hclm = 1
+        
+    flags_bits = Flags(f_reprint=0, f_hat_claim_from_pipe=hclm, 
+                       f_pipe=1, f_handler=0, f_badge_has_claimed_hat=0, 
+                       f_rst=rst, f_hat_holder=0, f_ink=0, f_nack=0, 
+                       f_ack=0, f_award=hawd)
+    flags_bytes = bitpack(flag_format, *flags_bits)
     flags = ord(flags_bytes[1])
     flags += (ord(flags_bytes[0]) << 8)
     
-    c = crc_check(pack(mating_format_nocrc, *(0x42, DEDICATED_BASE_ID, hat_id, 0, flags, 0x00, 0, 0, 0, 0, 0, 0)))
-    out_payload = pack(mating_format, 0x42, DEDICATED_BASE_ID, hat_id, 0, flags, 0x00, 0, 0, 0, 0, 0, 0, c)
+    out_payload_tuple = MatePayloadRaw(
+        proto_version=0x42,
+        from_addr=DEDICATED_BASE_ID,
+        hat_award_id=hid,
+        camo_id=0,
+        flags=flags,
+        achievements=0,
+        seen_count=0,
+        uber_seen_count=0,
+        odh_seen_count=0,
+        mate_count=0,
+        uber_mate_count=0,
+        odh_mate_count=0,
+        crc16=0
+    )
+    
+    c = crc_check(pack(mating_format_nocrc, *out_payload_tuple[:-1]))
+    out_payload_tuple = out_payload_tuple._replace(crc16=c)
+    out_payload = pack(mating_format, *out_payload_tuple)
     
     # TX: sync_bytes
     ser.write(map(chr, sync_bytes))
     # TX: out_payload
     ser.write(out_payload)
+    print ':::::TXed message:'
+    print '   Flags sent:'
+    print '    rst %d' % flags_bits.f_rst
+    print '    award %d' % flags_bits.f_award
+    print '    hatid %d' % out_payload_tuple.hat_award_id
+    
+
+def send_pipe_claim_hat(hat_id):
+    send_pipe_message(0, hat_id)
 
 def send_pipe_nrst():
-    flags_bits = (0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    flags_bytes = bitpack('u4u1u1u1u1u1u1u1u1u1u1u1u1', *flags_bits)
-    flags = ord(flags_bytes[1])
-    flags += (ord(flags_bytes[0]) << 8)
+    send_pipe_message(0, None)
 
-    c = crc_check(pack(mating_format_nocrc, *(0x42, DEDICATED_BASE_ID, 0xff, 0, flags, 0x00, 0, 0, 0, 0, 0, 0)))
-    out_payload = pack(mating_format, 0x42, DEDICATED_BASE_ID, 0xff, 0, flags, 0x00, 0, 0, 0, 0, 0, 0, c)
+def parse_payload(payload):
+    print ':Message RXed:::::'
+    mate_payload_tuple = MatePayloadRaw._make(unpack(mating_format, payload))
+    c = crc_check(pack(mating_format_nocrc, *mate_payload_tuple[:-1]))
+    if c != mate_payload_tuple.crc16:
+        print '    fail: bad crc'
+        return False
     
-    # TX: sync_bytes
-    ser.write(map(chr, sync_bytes))
-    # TX: out_payload
-    ser.write(out_payload)
-    print 'NRST sent'
+    flags0 = mate_payload_tuple.flags & 0xFF
+    flags1 = (mate_payload_tuple.flags & 0xFF00) >> 8
+    
+    in_flags = Flags._make(bitunpack(flag_format, [flags1, flags0]))
 
-def handle_input(payload):
-    mate_payload_tuple = unpack(mating_format, payload)
-    (proto_version,
-     from_addr,
-     hat_award_id,
-     camo_id,
-     flags,
-     achievements,
-     seen_count, uber_seen_count, odh_seen_count,
-     mate_count, uber_mate_count, odh_mate_count,
-     crc16) = mate_payload_tuple
-    c = crc_check(pack(mating_format_nocrc, proto_version, from_addr, hat_award_id, camo_id, flags, achievements, seen_count, uber_seen_count, odh_seen_count, mate_count, uber_mate_count, odh_mate_count))
-    if c != crc16:
-        print 'bad!'
-        return
+    print '   Flags received:'
+    print '    rst', in_flags.f_rst
+    print '    hat awarded', in_flags.f_hat_holder
+    print '    hat claimed', in_flags.f_badge_has_claimed_hat
     
-    flags0 = flags & 0xFF
-    flags1 = (flags & 0xFF00) >> 8
-    
-    (f_pad, f_reprint, f_hat_claim_from_pipe,
-     f_pipe, f_handler, f_badge_has_claimed_hat, f_rst,
-     f_hat_holder, f_ink, f_nack, f_ack, f_award,
-     f_unused) = bitunpack('u4u1u1u1u1u1u1u1u1u1u1u1u1', [flags1, flags0])
+    return MatePayload(*(mate_payload_tuple + in_flags))
 
-    print 'rst', f_rst
-    print 'hat awarded', f_hat_holder
-    print 'hat claimed', f_badge_has_claimed_hat
-
-    if not f_rst:
-        print "I think we're done"
-        return
-    
-    # So first we receive a RST from the badge. That's how we start our interaction
-    #  with it. 
-    # Then we send a message with ~RST and f_pipe and id=DEDICATED_BASE_ID
-    #  Badge then enters MS_PIPE_PAIRED
-    
-    # Send ~RST
-    send_pipe_nrst()
-    
-    # Now! The badge is right where we want it.
-    
-    badge_achievements = []
-    ach = achievements
-    
-    new_hat = False
-    
-    # Let's go ahead and read its achievements.
-    
-    for i in range(57):
-        if ach & 1:
-            badge_achievements.append(i)
-        ach = ach >> 1
-    print badge_achievements
-    
-    if f_hat_holder and f_badge_has_claimed_hat:
-        pass
-    elif f_hat_holder and not f_badge_has_claimed_hat and hat_award_id not in achievements_awarded:
-        # TIME TO AWARD IT, WOOOOOOOOO!!!!
-        print 'Already won, time to claim', hat_award_id
-        send_pipe_claim_hat(hat_award_id)
-        pass
-    else:
-        print 'Awarded are:', achievements_awarded
-        # Let's find an unawarded ACHIEVEMENT!
-        for achievement in badge_achievements[::-1]: # Work backwards.
-            if achievement not in achievements_awarded and achievement in hats:
-                # It's available!
-                print 'We can award hat %d' % achievement
-                send_pipe_claim_hat(achievement)
-                new_hat = True
-                break
-        
-    # Print a receipt.
-    print mate_payload_tuple
-    print 'id %d with hat %d' % (from_addr, hat_award_id)
-    # Done.
-
-
-with serial.Serial('/dev/ttyUSB0', 9600) as ser:
+# Never times out - listens forever to get a new message.
+# TODO: Probably should change that.
+def read_payload():
+    sync_position = 0
+    payload_len = 22
+    payload = ''
     while True:
         in_byte = ser.read()
         if sync_position < 4:
@@ -227,8 +169,68 @@ with serial.Serial('/dev/ttyUSB0', 9600) as ser:
                 payload = ''
         elif len(payload) == payload_len-1:
             payload += in_byte
-            handle_input(payload)
+            r = parse_payload(payload)
             payload = ''
             sync_position = 0
+            if r:
+                return r
+            else:
+                continue
         else:
             payload += in_byte
+    
+def read_and_handle_achievements(payload):
+    badge_achievements = []
+    new_hat = False
+    
+    result = None
+    
+    for i in hats.keys():
+        if (payload.achievements >> i) & 1:
+            badge_achievements.append(i)
+    print 'Achievements:', ', '.join(map(lambda a: hats[a][0], 
+                                     badge_achievements))
+    
+    if payload.f_hat_holder and payload.f_badge_has_claimed_hat:
+        # They already have a hat, and they've actually claimed it.
+        print 'They have a hat, and they have claimed it.'
+    elif payload.f_hat_holder and not payload.f_badge_has_claimed_hat and payload.hat_award_id not in achievements.awarded:
+        # AWARDING THE HAT!!!
+        print 'Already won, time to claim', payload.hat_award_id
+        send_pipe_claim_hat(payload.hat_award_id)
+        result = payload.hat_award_id
+    else:
+        # Let's try to find an unawarded achievement that they've
+        #  earned.
+        for a in hats.keys():
+            if a in badge_achievements and a not in achievements_awarded:
+                # It's available!
+                print 'We can award hat %d' % a
+                send_pipe_claim_hat(a)
+                result = a
+    # Finally, we just need to do a print.
+    # Let's let the main handle that.
+    return result
+    
+# There's 3 different things we can get from the badge:
+#   1. RST: means we reply with an ~RST
+#   2. NRST: means we read its achievements and print, possibly bestow.
+#   3. ACK or NACK: completes the bestow handshake.
+#   4. F_REPRINT: do a print again, regardless of whether we're in
+#       timeout.
+
+with serial.Serial('/dev/ttyUSB0', 9600) as ser:
+    while True:
+        p = read_payload()
+        if not p:
+            continue
+            
+        if p.f_rst:
+            send_pipe_nrst()
+        elif p.f_ack:
+            pass # Hat accepted
+        elif p.f_nack:
+            pass # Hat refused. Ungrateful jackass...
+        else:
+            # Got a NRST, time to read achievements.
+            read_and_handle_achievements(p)
